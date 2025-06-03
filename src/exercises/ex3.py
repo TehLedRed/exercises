b76dfd02d8ba756e940e87fc4bd71acb9a33bc1b
tehledred <ledred21@gmail.com>
2025-06-03 14:44:18 -0300

    Improvement

--- src/exercises/ex3.py
+++ src/exercises/ex3.py
@@ -1,11 +1,37 @@
 import math
+from typing import Generator
 
-def isArmstrong__(x):
-    
-    digits = [int(digit) for digit in str(x)]
+def isArmstrong__(x: list[int]) -> Generator[bool]:
+    """
+    Check if a given list of integers are Armstrong numbers.
 
-    k = math.floor(math.log(x, 10) + 1)
-    
-    return x == sum([digit ** k for digit in digits])
+    An Armstrong number is a number that is equal to the sum of its own digits
+    raised to the power of the number of digits.
+
+    Args:
+    x (list[int]): A list of integers to check.
+
+    Returns:
+    Generator: A generator object yielding True for Armstrong numbers and False
+    for non-Armstrong numbers.
+
+    Example:
+    >>> list(isArmstrong__([153, 370, 371, 407]))
+    [True, True, True, True]
+
+    >>> list(isArmstrong__([1634, 8208, 9474, 54748]))
+    [True, True, True, True]
+
+    >>> list(isArmstrong__([1633, 8209, 9475, 54749]))
+    [False, False, False, False]
+    """
+    if not x:
+        return
+    for num in x:
+        k = math.floor(math.log(num, 10) + 1)
+        digits = [int(digit) for digit in str(num)]
+
+        yield num == sum([digit ** k for digit in digits])
 
 
+print(list(isArmstrong__(['153'])))
