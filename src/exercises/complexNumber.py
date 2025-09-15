7ba8d446f39ad25691bc40104421ba744191f183
tehledred <ledred21@gmail.com>
2025-09-15 12:28:33 -0300

    Adapt to Fazenda's test

--- src/exercises/complexNumber.py
+++ src/exercises/complexNumber.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from math import cos, exp, sin
+from math import cos, exp, sin, isclose
 
 NUMBER = int | float
 
@@ -18,37 +18,50 @@ class complexNumber(object):
         a: NUMBER,
         b: NUMBER,
     ) -> None:
-        self.a = a
-        self.b = b
+        self.real = a
+        self.imaginary = b
         self.complex = a + b * 1j
 
-    def __conjugate__(self) -> complexNumber:
-        return complexNumber(self.a, -self.b)
+    def __eq__(
+        self,
+        other: object,
+    ) -> bool:
+        if not isinstance(other, complexNumber):
+            raise ValueError("Can only compare with another complexNumber instance.")
+
+        return isclose(self.real, other.real) and isclose(self.imaginary, other.imaginary)
+
+    def __repr__(self) -> str:
+        return f"complexNumber({self.real}, {self.imaginary})"
+
+    def conjugate(self) -> complexNumber:
+        return complexNumber(self.real, -self.imaginary)
 
     def __abs__(self) -> float:
-        return float((self.a**2 + self.b**2) ** 0.5)
+        return float((self.real**2 + self.imaginary**2) ** 0.5)
 
     def __absoluteSq__(self) -> float:
-        return self.a**2 + self.b**2
+        return self.real**2 + self.imaginary**2
 
     def __add__(self, other: complexNumber) -> complexNumber:
-        return complexNumber(self.a + other.a, self.b + other.b)
+        return complexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __sub__(self, other: complexNumber) -> complexNumber:
-        return complexNumber(self.a - other.a, self.b - other.b)
+        return complexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __mul__(self, other: complexNumber) -> complexNumber:
         return complexNumber(
-            self.a * other.a - self.b * other.b, self.b * other.a + self.a * other.b
+            self.real * other.real - self.imaginary * other.imaginary,
+            self.imaginary * other.real + self.real * other.imaginary,
         )
 
     def __reciprocal__(self) -> complexNumber:
-        denom = self.a**2 + self.b**2
-        return complexNumber(self.a / denom, -self.b / denom)
+        denom = self.real**2 + self.imaginary**2
+        return complexNumber(self.real / denom, -self.imaginary / denom)
 
-    def __div__(self, other: complexNumber) -> complexNumber:
+    def __truediv__(self, other: complexNumber) -> complexNumber:
         return self.__mul__(other.__reciprocal__())
 
-    def __exponentiation__(self) -> complexNumber:
-        exp_a = exp(self.a)
-        return complexNumber(exp_a * cos(self.b), exp_a * sin(self.b))
+    def exp(self) -> complexNumber:
+        exp_a = exp(self.real)
+        return complexNumber(exp_a * cos(self.imaginary), exp_a * sin(self.imaginary))
--- tests/test_complexNumber.py
+++ tests/test_complexNumber.py
@@ -1,62 +1,202 @@
+from math import e, log, pi
+from typing import TypedDict
+from typing_extensions import Unpack
+
+from pytest import approx
 from exercises.complexNumber import complexNumber
 
 
-def test_init() -> None:
-    z = complexNumber(3, 4)
-    assert z.a == 3 and z.b == 4
+class ApproxKwargs(TypedDict, total=False):
+    rel: float
+    abs: float
+    nan_ok: bool
 
 
-def test_conjugate() -> None:
-    z = complexNumber(3, 4)
-    conj = z.__conjugate__()
-    assert conj.a == 3 and conj.b == -4
+def complexNumber_approx(c1: complexNumber, c2: complexNumber, **kwargs: Unpack[ApproxKwargs]) -> bool:
+    return approx(c1.real, **kwargs) == approx(c2.real, **kwargs) and approx(
+        c1.imaginary, **kwargs
+    ) == approx(c2.imaginary, **kwargs)
 
 
-def test_abs() -> None:
-    z = complexNumber(3, 4)
-    assert z.__abs__() == 5
+def test_real_part_of_a_purely_real_number() -> None:
+    assert 1 == complexNumber(1, 0).real
 
 
-def test_absoluteSq() -> None:
-    z = complexNumber(3, 4)
-    assert z.__absoluteSq__() == 25
+def test_real_part_of_a_purely_imaginary_number() -> None:
+    assert 0 == complexNumber(0, 1).real
 
 
-def test_add() -> None:
-    z1 = complexNumber(3, 4)
-    z2 = complexNumber(5, 6)
-    result = z1.__add__(z2)
-    assert result.a == 8 and result.b == 10
+def test_real_part_of_a_number_with_real_and_imaginary_part() -> None:
+    assert 1 == complexNumber(1, 2).real
 
 
-def test_sub() -> None:
-    z1 = complexNumber(3, 4)
-    z2 = complexNumber(5, 6)
-    result = z1.__sub__(z2)
-    assert result.a == -2 and result.b == -2
+# Imaginary part
 
 
-def test_mul() -> None:
-    z1 = complexNumber(3, 4)
-    z2 = complexNumber(5, 6)
-    result = z1.__mul__(z2)
-    assert result.a == -9 and result.b == 38
+def test_imaginary_part_of_a_purely_real_number() -> None:
+    assert 0 == complexNumber(1, 0).imaginary
 
 
-def test_reciprocal() -> None:
-    z = complexNumber(3, 4)
-    recip = z.__reciprocal__()
-    assert abs(recip.a - 0.12) < 0.01 and abs(recip.b - (-0.16)) < 0.01
+def test_imaginary_part_of_a_purely_imaginary_number() -> None:
+    assert 1 == complexNumber(0, 1).imaginary
 
 
-def test_div() -> None:
-    z1 = complexNumber(3, 4)
-    z2 = complexNumber(5, 6)
-    result = z1.__div__(z2)
-    assert abs(result.a - 0.6393) < 0.01 and abs(result.b - 0.0328) < 0.01
+def test_imaginary_part_of_a_number_with_real_and_imaginary_part() -> None:
+    assert 2 == complexNumber(1, 2).imaginary
 
 
-def test_exponentiation() -> None:
-    z = complexNumber(1, 0)
-    result = z.__exponentiation__()
-    assert abs(result.a - 2.718) < 0.01 and abs(result.b) < 0.01
+# Equality
+
+
+def test_equality_first() -> None:
+    assert complexNumber(1, 0) == complexNumber(1, 0)
+
+
+def test_equality_second() -> None:
+    assert complexNumber(1, 0) != complexNumber(2, 0)
+
+
+# Arithmetic
+
+# Addition
+
+
+def test_add_purely_real_numbers() -> None:
+    assert complexNumber(1, 0) + complexNumber(2, 0) == complexNumber(3, 0)
+
+
+def test_add_purely_imaginary_numbers() -> None:
+    assert complexNumber(0, 1) + complexNumber(0, 2) == complexNumber(0, 3)
+
+
+def test_add_numbers_with_real_and_imaginary_part() -> None:
+    assert complexNumber(1, 2) + complexNumber(3, 4) == complexNumber(4, 6)
+
+
+# Subtraction
+
+
+def test_subtract_purely_real_numbers() -> None:
+    assert complexNumber(1, 0) - complexNumber(2, 0) == complexNumber(-1, 0)
+
+
+def test_subtract_purely_imaginary_numbers() -> None:
+    assert complexNumber(0, 1) - complexNumber(0, 2) == complexNumber(0, -1)
+
+
+def test_subtract_numbers_with_real_and_imaginary_part() -> None:
+    assert complexNumber(1, 2) - complexNumber(3, 4) == complexNumber(-2, -2)
+
+
+# Multiplication
+
+
+def test_imaginary_unit() -> None:
+    assert complexNumber(0, 1) * complexNumber(0, 1) == complexNumber(-1, 0)
+
+
+def test_multiply_purely_real_numbers() -> None:
+    assert complexNumber(1, 0) * complexNumber(2, 0) == complexNumber(2, 0)
+
+
+def test_multiply_purely_imaginary_numbers() -> None:
+    assert complexNumber(0, 1) * complexNumber(0, 2) == complexNumber(-2, 0)
+
+
+def test_multiply_numbers_with_real_and_imaginary_part() -> None:
+    assert complexNumber(1, 2) * complexNumber(3, 4) == complexNumber(-5, 10)
+
+
+# Division
+
+
+def test_divide_purely_real_numbers() -> None:
+    assert complexNumber(1, 0) / complexNumber(2, 0) == complexNumber(0.5, 0)
+
+
+def test_divide_purely_imaginary_numbers() -> None:
+    assert complexNumber(0, 1) / complexNumber(0, 2) == complexNumber(0.5, 0)
+
+
+def test_divide_numbers_with_real_and_imaginary_part() -> None:
+    assert complexNumber(1, 2) / complexNumber(3, 4) == complexNumber(0.44, 0.08)
+
+
+# Absolute value
+
+
+def test_absolute_value_of_a_positive_purely_real_number() -> None:
+    assert abs(complexNumber(5, 0)) == 5
+
+
+def test_absolute_value_of_a_negative_purely_real_number() -> None:
+    assert abs(complexNumber(-5, 0)) == 5
+
+
+def test_absolute_value_of_a_purely_imaginary_number_with_positive_imaginary_part() -> (
+    None
+):
+    assert abs(complexNumber(0, 5)) == 5
+
+
+def test_absolute_value_of_a_purely_imaginary_number_with_negative_imaginary_part() -> (
+    None
+):
+    assert abs(complexNumber(0, -5)) == 5
+
+
+def test_absolute_value_of_a_number_with_real_and_imaginary_part() -> None:
+    assert abs(complexNumber(3, 4)) == 5
+
+
+# complexNumber conjugate
+
+
+def test_conjugate_a_purely_real_number() -> None:
+    assert complexNumber(5, 0).conjugate() == complexNumber(5, 0)
+
+
+def test_conjugate_a_purely_imaginary_number() -> None:
+    assert complexNumber(0, 5).conjugate() == complexNumber(0, -5)
+
+
+def test_conjugate_a_number_with_real_and_imaginary_part() -> None:
+    assert complexNumber(1, 1).conjugate() == complexNumber(1, -1)
+
+
+# complexNumber exponential function
+
+
+def test_exponential_of_0() -> None:
+    assert complexNumber(0, 0).exp() == complexNumber(1, 0)
+
+
+def test_euler_s_identity_formula() -> None:
+    assert complexNumber_approx(complexNumber(0, pi).exp(), complexNumber(-1, 0), abs=0.01)
+
+
+def test_exponential_of_a_purely_real_number() -> None:
+    assert complexNumber_approx(complexNumber(1, 0).exp(), complexNumber(e, 0), abs=0.01)
+
+
+def test_exponential_of_a_number_with_real_and_imaginary_part() -> None:
+    assert complexNumber_approx(complexNumber(log(2), pi).exp(), complexNumber(-2, 0), abs=0.01)
+
+
+def test_exponential_resulting_in_a_number_with_real_and_imaginary_part() -> None:
+    assert complexNumber_approx(complexNumber(log(2) / 2, pi / 4).exp(), complexNumber(1, 1), abs=0.01)
+
+
+# Additional tests for this track
+
+
+def test_equality_of_complexNumber_numbers() -> None:
+    assert complexNumber(1, 2) == complexNumber(1, 2)
+
+
+def test_inequality_of_real_part() -> None:
+    assert complexNumber(1, 2) != complexNumber(2, 2)
+
+
+def test_inequality_of_imaginary_part() -> None:
+    assert complexNumber(1, 2) != complexNumber(1, 1)
